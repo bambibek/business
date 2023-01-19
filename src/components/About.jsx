f307d1e63d324b85d80d59bba76f38c7624d32a6
Bibek Bam <bibek2bam@gmail.com>
2023-01-19 12:46:51 +0200

    completed contact Us and about components, added images to library and miron style fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container py-5 my-5">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="text-primary fw-bold mb-4">ABOUT US</h1>
+            <p className="lead mb-4">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Consequuntur dicta earum corrupti sapiente debitis laudantium
+              consequatur beatae? Inventore laudantium corporis aliquam, dolorem
+              excepturi possimus suscipit in nostrum ab nihil error veniam
+              placeat cumque? Animi consectetur explicabo nemo iusto amet ipsa
+              consequatur inventore facilis sunt, voluptatibus velit similique
+              quod atque illo voluptatem quo. Eaque est nemo, quos tempora velit
+              beatae ea consectetur deserunt saepe aliquid repellat qui, cumque
+              recusandae excepturi, nam commodi. Asperiores, labore. Doloribus
+              quasi fuga, optio explicabo sed earum et mollitia quae maiores
+              officia illo vero. Voluptas, in! Quibusdam soluta hic minus
+              tenetur dolores ducimus sapiente eum explicabo eaque?
+            </p>
+            <NavLink to="/contact" className="btn btn-outline-primary px-3">
+              Contact Us
+            </NavLink>
+          </div>
+          <div className="col-md-6 d-flex justify-content-center ">
+            <img
+              src="/assets/images/aboutUs.png"
+              alt="About Us"
+              height="400px"
+              width="400px"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,73 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="container mb-5">
+        <div className="row">
+          <div className="col-12 text-center py-4 my-4">
+            <h1>Ask Us About Your Confusion</h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md 5 d-flex justify-content-center">
+            <img
+              src="/assets/images/contact.png"
+              alt="Contact Us"
+              height="350px"
+              width="350px"
+            />
+          </div>
+          <div className="col-md-6">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="exampleForm" className="form-label">
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleForm"
+                  placeholder="John Doe"
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="name@example.com"
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlTextarea1"
+                  className="form-label"
+                >
+                  Example textarea
+                </label>
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-outline-primary">
+                Send Message
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
